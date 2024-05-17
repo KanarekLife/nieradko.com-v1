b166e400b5ad1f9985ce5a16c49e66826ef97d99
Stanisław Nieradko <stanislaw@nieradko.com>
2024-05-17 18:06:41 +0200

    wip

---
+++ src/content/blog/2024-05-12-hello-world/index.md
@@ -0,0 +1,14 @@
+---
+title: "Hello World"
+tags: ["programming"]
+created: 2024-05-12
+image:
+    src: ./bg.jpg
+    alt: Featured image
+---
+
+Hey, there!
+
+My name is Stanisław Nieradko and I am happy to welcome you to my new blog! I wanted to create some kind of blog, for me to share any interesting findings and knowledge, for some time but I never could bring myself to do it.
+
+![Image](./img1.jpg)
---
+++ src/content/config.ts
@@ -0,0 +1,22 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+	type: "content",
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			tags: z.array(z.string()).default([]),
+			created: z.coerce.date(),
+			draft: z.boolean().default(true),
+			image: z
+				.object({
+					src: image(),
+					alt: z.string(),
+				})
+				.optional(),
+		}),
+});
+
+export const collections = {
+	blog: blogCollection,
+};
