21a7c9d4f1b23f4d641070e48c3ec509aa3e5a1e
Stanisław Nieradko <stanislaw@nieradko.com>
2024-06-16 14:48:27 +0200

    tweaked blog post page

--- astro.config.ts
+++ astro.config.ts
@@ -3,5 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
 	integrations: [tailwind({ applyBaseStyles: false })],
-	site: "https://nieradko.com",
+	site: "https://nieradko.com"
 });
